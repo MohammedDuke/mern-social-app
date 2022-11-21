2456a2eedb2947eef7ffab378da668fdf477b091
Mohammed Ahmed <hamoahmed8055@gmail.com>
2022-11-21 17:08:21 +0200

    create main file for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "screen/homePage";
+import LoginPage from "screen/loginPage";
+import ProfilePage from "screen/profilePage";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/home" element={<HomePage />}></Route>
+          <Route path="/profile/:userId" element={<ProfilePage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
